7207a2363b886f5404519ccb9bb427e803383256
Maxim <eremenko98@yandex.ru>
2020-05-16 16:02:27 +0700

    Add inf_input function

--- makefile
+++ makefile
@@ -1,10 +1,11 @@
 CFLAGS = -g -Wall -Werror
 CC = g++
 EXECUTABLE = prog
-SOURCES = src/main.cpp src/split.cpp
+SOURCES = src/main.cpp src/readCircle.cpp
 OBJECTS = $(SOURCES: .c=build/.o)
 
 all: $(SOURCES) $(EXECUTABLE)
+
 $(EXECUTABLE): $(OBJECTS)
 	$(CC) $(OBJECTS) -o $@
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,144 @@
+#include "readCircle.h"
 #include <cstdio>
 #include <cstdlib>
+#include <string>
+#include <exception>
 #include <iostream>
-#include "readCircle.h"
+#include <vector>
 #define N 256
+#define PI 3.14
 using namespace std;
+
 struct circle {
-    char figure[N];
+    string figure;
     int x, y;
     double r;
+    double perimeter;
+    double area;
 };
-int main()
+
+struct triangle {
+    string figure;
+    vector<int> xy;
+    double perimeter;
+    double area;
+};
+
+struct figures
+{
+    circle a;
+    triangle b;
+};
+
+void inf_input()
 {
     int num = 0;
     cout << "Enter N = ";
     cin >> num;
     char tmp[N];
-    char tmp1[N];
+    char fig[N];
+    char coo[N];
+    vector<string> res_str;
+    vector<figures> result;
+    string str, coor;
     for (int i = 0; i < num; i++) {
         cin.clear();
-        while (cin.get() != '\n');
-        cout << "enter figure(x y, r)" << endl;
+        while (cin.get() != '\n')
+            ;
+        cout << "enter circle(x y, r) or triangle ((x1 y1, x2 y2, x3 y3, x1 y1))" << endl;
+        int meetings = 0;
         cin.getline(tmp, 100, '(');
-        cout << tmp << endl;
-        cin.getline(tmp1, 100,')');
-        cout << tmp1 << endl;
-        string str = string(tmp);
-        string coor = string(tmp1);
-        split(coor, ',');
+        cin.getline(coo, 100);
+        _strlwr(tmp);
+        if (strcmp("triangle", tmp) == 0)
+        {
+            int size = strlen(coo);
+            try {
+                if (coo[0] == '(' && coo[size - 1] == ')' && coo[size - 2] == ')')
+                {
+                    coo[0] = ' ';
+                    coo[size - 1] = ' ';
+                    coo[size - 2] = ' ';
+                }
+                else
+                    throw "invalid input format";
+            }
+            catch (const char* exception)
+            {
+                std::cerr << "Error: " << exception << '\n';
+                goto next;
+            }
+        }
+        if (strcmp("circle", tmp) == 0)
+        {
+            int size = strlen(coo);
+            try {
+                if (coo[size - 1] == ')')
+                {
+                    coo[size - 1] = ' ';
+                }
+                else
+                    throw "invalid input format";
+            }
+            catch (const char* exception)
+            {
+                std::cerr << "Error: " << exception << '\n';
+                goto next;
+            }
+        }
+        try {
+            if (strcmp("circle", tmp) != 0 && strcmp("triangle", tmp) != 0)
+            {
+                throw "unknown figure";
+            }
+        }
+        catch (const char* exception)
+        {
+            std::cerr << "Error: " << exception << '\n';
+            goto next;
+        }
+        str = string(tmp);
+        coor = string(coo);
+        res_str = split(coor, ',');
+        if (strcmp("circle", tmp) == 0)
+        {
+            try {
+                if (res_str.size() != 3)
+                {
+                    throw "invalid input format";
+                }
+            }
+            catch (const char* exception)
+            {
+                std::cerr << "Error: " << exception << '\n';
+                goto next;
+            }
+        }
+        if (strcmp("triangle", tmp) == 0)
+        {
+            try {
+                if (res_str.size() != 8)
+                {
+                    throw "invalid input format";
+                }
+            }
+            catch (const char* exception)
+            {
+                std::cerr << "Error: " << exception << '\n';
+                goto next;
+            }
+        }
+        fill_struct(res_str, tmp, result);
+    next:
+        str = "";
+        coor = "";
     }
+    cout << endl;
+    print(result);
+}
 
+int main()
+{
+    inf_input();
     return 0;
 } 
