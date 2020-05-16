267c88d1e39f203dc6ca2ef0b679dc888744fb38
Maxim <eremenko98@yandex.ru>
2020-05-16 16:41:14 +0700

    some more changes to travis-ci

--- src/geom.h
+++ src/geom.h
@@ -6,6 +6,6 @@ using namespace std;
 double areas(std::vector<int> a);
 double lenght_of_segment(int x1, int y1, int x2, int y2);
 double perimetrs(std::vector<int> a);
-char *_strlwr(char* str);
+char* _strlwr(char* str);
 
 #endif 
