526409877aa6ee61198bda12aaa416d915ec4f1f
Maxim <eremenko98@yandex.ru>
2020-06-11 18:57:04 +0700

    Add test cases for circle intersections and triangle intersectios

--- test/test_intersect.cpp
+++ test/test_intersect.cpp
@@ -1,18 +1,59 @@
- #define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "intesections.h"
 #include "read-fill.h"
 #include <iostream>
 #include <string.h>
+#include <vector>
 
-TEST_CASE("intersection", "[triangles]")
+TEST_CASE("intersections of triangles", "[triangles]")
 {
     SECTION("counting the area of a triangle by three points")
     {
         REQUIRE(area_int(2, 3, 6, 7, 8, 10) == 4);
     }
-    SECTION("checkint the location of points")
+    SECTION("checking the location of points")
     {
         REQUIRE(inters_segment(3, 4, 6, 7) == false);
+        REQUIRE(inters_segment(5, 5, 5, 5) == true);
+    }
+    SECTION("checking the location of segments")
+    {
+        REQUIRE(inter_segments(1, 0, 2, 1, 1, 0, 2, 0) == true);
+        REQUIRE(inter_segments(-1, 1, -1, 2, 1, 1, 2, 2) == false);
+    }
+    vector<int> a = {2, 2, -1, -2, 3, -2, 2, 2};
+    vector<int> b = {3, 3, 1, -1, 5, -1, 3, 3};
+    vector<int> c = {-2, 2, -3, 4, -4, 2, -2, 2};
+    triangle* a_ = new triangle;
+    triangle* b_ = new triangle;
+    triangle* c_ = new triangle;
+    a_->figure = "triangle";
+    a_->xy = a;
+    b_->figure = "triangle";
+    b_->xy = b;
+    c_->figure = "triangle";
+    c_->xy = c;
+    REQUIRE(intersections_triangles(a_, b_) == true);
+    REQUIRE(intersections_triangles(a_, c_) == false);
+}
+
+TEST_CASE("intersections of circles", "[circles]")
+{
+    circle* a_ = new circle;
+    circle* b_ = new circle;
+    circle* c_ = new circle;
+    a_->figure = b_->figure = c_->figure = "circle";
+    a_->x = 0;
+    a_->y = 0;
+    a_->r = 3;
+    b_->x = 1;
+    b_->y = 1;
+    b_->r = 2;
+    c_->x = 10;
+    c_->y = 10;
+    c_->r = 3;
+    REQUIRE(intersections_circles(a_, b_) == true);
+    REQUIRE(intersections_circles(a_, c_) == false);
     }
 } 
